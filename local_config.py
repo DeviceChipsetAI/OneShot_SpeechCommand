574297ea02b06c74035914de7e1e10fdba78a6ea
hermitwang <hermitcranecn@gmail.com>
2018-12-15 15:15:36 +0800

    1, move local config from the notebook to a local config python file 2, minor errors fixed

---
+++ local_config.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+default_split='/'
+#for mac
+speech_data_dir="/Users/hermitwang/Downloads/speech_dataset"
+default_model_path="/Users/hermitwang/Downloads/pretrained/speech_siamese"
+default_processed_data_path="/Users/hermitwang/Downloads/pretrained/speech_siamese"
+#for ubuntu
+#speech_data_dir="/home/hermitwang/TrainingData/datasets/speech_dataset"
+#default_model_path="/home/hermitwang/TrainingData/pretrained/speech_siamese"
+#default_processed_data_path="/home/hermitwang/TrainingData/pretrained/speech_siamese"
+#for windows 7 gpu
+#default_split='\\'
+#speech_data_dir="D:\\Projects\\datasets\\speech_dataset"
+#default_model_path="D:\Projects\\pretrained\\speech_siamese"
+#default_processed_data_path="D:\\Projects\\pretrained\\speech_siamese"
+#for windows of work
+#speech_data_dir="D:\\HermitWang\\DeepLearning\\dataset\\speech_dataset"
+#default_model_path="D:\\HermitWang\\DeepLearning\\dataset\\trained\\siamese"
+#for himl 
+#speech_data_dir="/home/hermitwang/Projects/datasets/speech_dataset"
+#default_model_path="/home/hermitwang/Projects/pretrained/speech_siamese"
+#default_processed_data_path="/home/hermitwang/Projects/pretrained/speech_siamese"
+
